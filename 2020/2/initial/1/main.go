370281d87ad3e5d466462629bdfc81ae3bf4e13e
danvolchek <danvolchek@users.noreply.github.com>
2020-12-01 21:12:54 -0800

    2020/2: add initial solution

--- 2020/2/initial/1/main.go
+++ 2020/2/initial/1/main.go
@@ -6,17 +6,75 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 )
+
+type thingy struct {
+	min, max int
+	char uint8
+	pass string
+}
 
 func parse(r io.Reader) {
 	csvReader := csv.NewReader(r)
 
+	csvReader.Comma = ':'
 	rows, err := csvReader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(rows)
+	matching := 0
+
+	for _, row := range rows {
+		t := thingy{}
+		first := row[0]
+
+		parts := strings.Split(first, " ")
+
+		amount := parts[0]
+
+		nums := strings.Split(amount, "-")
+
+		min, err := strconv.Atoi(nums[0])
+		if err != nil {
+			panic(err)
+		}
+
+		t.min = min
+
+		max, err := strconv.Atoi(nums[1])
+		if err != nil {
+			panic(err)
+		}
+
+		t.max = max
+
+		t.char = parts[1][0]
+
+		t.pass = row[1]
+
+		found := count(t.char, t.pass)
+
+		if found >= t.min && found <= t.max {
+			matching++
+		}
+	}
+
+	fmt.Println(matching)
+
+}
+
+func count(char uint8, pass string) int {
+	num := 0
+	for i := 0; i < len(pass); i++{
+		if pass[i] == char {
+			num++
+		}
+	}
+
+	return num
 }
 
 func main() {
--- 2020/2/initial/2/main.go
+++ 2020/2/initial/2/main.go
@@ -6,17 +6,67 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 )
+
+type thingy struct {
+	min, max int
+	char uint8
+	pass string
+}
 
 func parse(r io.Reader) {
 	csvReader := csv.NewReader(r)
 
+	csvReader.Comma = ':'
 	rows, err := csvReader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(rows)
+	matching := 0
+
+	for _, row := range rows {
+		t := thingy{}
+		first := row[0]
+
+		parts := strings.Split(first, " ")
+
+		amount := parts[0]
+
+		nums := strings.Split(amount, "-")
+
+		min, err := strconv.Atoi(nums[0])
+		if err != nil {
+			panic(err)
+		}
+
+		t.min = min
+
+		max, err := strconv.Atoi(nums[1])
+		if err != nil {
+			panic(err)
+		}
+
+		t.max = max
+
+		t.char = parts[1][0]
+
+		t.pass = strings.TrimSpace(row[1])
+
+
+		a := t.pass[t.min - 1] == t.char
+		b := t.pass[t.max - 1] == t.char
+
+		if a && !b || b && !a {
+			matching++
+			fmt.Println(t.min, t.max, t.char, t.pass)
+		}
+	}
+
+	fmt.Println(matching)
+
 }
 
 func main() {
