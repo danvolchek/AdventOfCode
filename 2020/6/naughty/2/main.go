70d607d50f1af853f781bde07563ecfed52acb5e
danvolchek <danvolchek@users.noreply.github.com>
2020-12-05 14:47:15 -0800

    2020/6: add template

---
+++ 2020/6/naughty/2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"path"
+)
+
+func input() *os.File {
+    input, err := os.Open(path.Join("2020", "6", "input.txt"))
+    if err != nil {
+        panic(err)
+    }
+
+    return input
+}
+
+func solve(r io.Reader) {
+	csvReader := csv.NewReader(r)
+
+	rows, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(rows)
+}
+
+func main() {
+	solve(input())
+}
