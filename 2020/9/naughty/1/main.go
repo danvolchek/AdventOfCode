1158239f2a92b2fa0d6da869fa5ba4850d7478d5
danvolchek <danvolchek@users.noreply.github.com>
2020-12-08 21:20:18 -0800

    2020/9: add naughty solutions, input

--- 2020/9/naughty/1/main.go
+++ 2020/9/naughty/1/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
 )
 
 func input() *os.File {
@@ -17,20 +18,49 @@ func input() *os.File {
 	return input
 }
 
+func find(target int, nums []int) bool {
+	for _, i := range nums {
+		for _, j := range nums {
+			if i == j {
+				continue
+			}
+
+			if i+j == target {
+				return true
+			}
+		}
+	}
+
+	return false
+}
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	var numbers []int
 	for scanner.Scan() {
-		row := scanner.Bytes()
+		row := scanner.Text()
 
-		fmt.Println(row)
+		v, _ := strconv.Atoi(row)
+		numbers = append(numbers, v)
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	preamble := 25
+
+	for i := preamble; i < len(numbers); i++ {
+		if !find(numbers[i], numbers[i-preamble:i]) {
+			fmt.Println(numbers[i])
+			return
+		}
+	}
+
+	panic("nothing")
 }
 
 func main() {
+	//solve(strings.NewReader("35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576"))
 	solve(input())
 }
--- 2020/9/naughty/2/main.go
+++ 2020/9/naughty/2/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
 )
 
 func input() *os.File {
@@ -20,17 +21,44 @@ func input() *os.File {
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	var numbers []int
 	for scanner.Scan() {
-		row := scanner.Bytes()
+		row := scanner.Text()
 
-		fmt.Println(row)
+		v, _ := strconv.Atoi(row)
+		numbers = append(numbers, v)
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	target := 22406676
+
+	fmt.Println(len(numbers))
+	for k := 0; k < len(numbers)-1; k++ {
+
+		for l := 1; l < len(numbers)-k; l++ {
+
+			//fmt.Println(k, l, "m is max", l-1)
+			sum := 0
+			for m := 0; m < l; m++ {
+				sum += numbers[k+m]
+			}
+
+			if sum == target {
+				fmt.Println(numbers[k], numbers[k+l-1], numbers[k]+numbers[k+l-2])
+				return
+			}
+
+		}
+
+	}
+
+	panic("nothing")
 }
 
 func main() {
+	//solve(strings.NewReader("35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576"))
 	solve(input())
 }
