4ad9f3ddf3869b0be082854114fad68abb98a017
danvolchek <danvolchek@users.noreply.github.com>
2020-12-13 21:29:18 -0800

    2020/14: add naughty solutions, input

--- 2020/14/naughty/1/main.go
+++ 2020/14/naughty/1/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"os"
 	"path"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func input() *os.File {
@@ -17,20 +20,70 @@ func input() *os.File {
 	return input
 }
 
+var reg = regexp.MustCompile(`mem\[(\d+)\] = (\d+)`)
+
+func set(memory map[int]int, mask string, memIndex int, memValue int) {
+	val := 0
+
+	for i := 0; i < 36; i++ {
+		if mask[36-i-1] != 'X' {
+			if mask[36-i-1] == '1' {
+				val |= 1 << i
+			}
+			continue
+		}
+
+		val |= ((memValue >> i) & 1) << i
+	}
+
+	memory[memIndex] = val
+}
+
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
+
+	var mask string
+
+	memory := make(map[int]int)
 
 	for scanner.Scan() {
 		row := scanner.Text()
 
-		fmt.Println(row)
+		if strings.Index(row, "mask") == 0 {
+			mask = strings.Split(row, " = ")[1]
+			continue
+		} else {
+			parts := reg.FindStringSubmatch(row)
+
+			index, err := strconv.Atoi(parts[1])
+			if err != nil {
+				panic(err)
+			}
+
+			value, err := strconv.Atoi(parts[2])
+			if err != nil {
+				panic(err)
+			}
+
+			set(memory, mask, index, value)
+
+		}
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	sum := 0
+
+	for _, val := range memory {
+		sum += val
+	}
+
+	fmt.Println(sum)
 }
 
 func main() {
+	solve(strings.NewReader("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0"))
 	solve(input())
 }
--- 2020/14/naughty/2/main.go
+++ 2020/14/naughty/2/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"os"
 	"path"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func input() *os.File {
@@ -17,20 +20,118 @@ func input() *os.File {
 	return input
 }
 
+var reg = regexp.MustCompile(`mem\[(\d+)\] = (\d+)`)
+
+func addressToInt(addressSpace string) int {
+	val := 0
+	for i := 0; i < 36; i++ {
+		if addressSpace[i] == '1' {
+			val |= 1 << (36 - i - 1)
+		}
+
+	}
+
+	return val
+}
+
+func intToAddress(memIndex int) string {
+	val := ""
+
+	for i := 0; i < 36; i++ {
+		if (memIndex>>i)&1 == 1 {
+			val = "1" + val
+		} else {
+			val = "0" + val
+		}
+
+	}
+
+	return val
+}
+
+func setR(memory map[int]int, index int, addressSpace string, memValue int) {
+	for i := index; i < 36; i++ {
+		if addressSpace[i] != 'X' {
+			continue
+		}
+
+		for _, val := range []string{"0", "1"} {
+			newAddr := addressSpace[:i] + val + addressSpace[i+1:]
+			//fmt.Println("recurse on ", newAddr)
+			setR(memory, i+1, newAddr, memValue)
+		}
+		return
+
+	}
+
+	//fmt.Println("setting", addressSpace, addressToInt(addressSpace))
+
+	addr := addressToInt(addressSpace)
+	memory[addr] = memValue
+
+}
+
+func set(memory map[int]int, mask string, memIndex int, memValue int) {
+	indexAsStr := intToAddress(memIndex)
+
+	for i := 0; i < 36; i++ {
+		if mask[i] == '0' {
+			continue
+		} else if mask[i] == '1' {
+			indexAsStr = indexAsStr[:i] + "1" + indexAsStr[i+1:]
+		} else {
+			indexAsStr = indexAsStr[:i] + "X" + indexAsStr[i+1:]
+		}
+	}
+
+	setR(memory, 0, indexAsStr, memValue)
+}
+
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
+
+	var mask string
+
+	memory := make(map[int]int)
 
 	for scanner.Scan() {
 		row := scanner.Text()
 
-		fmt.Println(row)
+		if strings.Index(row, "mask") == 0 {
+			mask = strings.Split(row, " = ")[1]
+			continue
+		} else {
+			parts := reg.FindStringSubmatch(row)
+
+			index, err := strconv.Atoi(parts[1])
+			if err != nil {
+				panic(err)
+			}
+
+			value, err := strconv.Atoi(parts[2])
+			if err != nil {
+				panic(err)
+			}
+
+			set(memory, mask, index, value)
+
+		}
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	sum := 0
+
+	for _, val := range memory {
+		sum += val
+	}
+
+	fmt.Println(sum)
 }
 
 func main() {
+	solve(strings.NewReader("mask = 000000000000000000000000000000X1001X\nmem[42] = 100\nmask = 00000000000000000000000000000000X0XX\nmem[26] = 1"))
 	solve(input())
 }
