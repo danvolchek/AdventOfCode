dab4252b778b37e60b39aa9910c198d775061ee1
danvolchek <danvolchek@users.noreply.github.com>
2020-12-15 21:31:03 -0800

    2020/5: improve readability

--- 2020/5/nice/2/main.go
+++ 2020/5/nice/2/main.go
@@ -39,11 +39,6 @@ func decode(encoded []byte) int {
 	return decoded
 }
 
-func contains(m map[int]bool, val int) bool {
-	_, ok := m[val]
-	return ok
-}
-
 func solve(boardingPasses [][]byte) int {
 	ids := make(map[int]bool, len(boardingPasses))
 
@@ -52,11 +47,11 @@ func solve(boardingPasses [][]byte) int {
 	}
 
 	for i := range ids {
-		if !contains(ids, i-1) && contains(ids, i-2) {
+		if !ids[i-1] && ids[i-2] {
 			return i - 1
 		}
 
-		if !contains(ids, i+1) && contains(ids, i+2) {
+		if !ids[i+1] && ids[i+2] {
 			return i + 1
 		}
 	}
