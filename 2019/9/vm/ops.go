b8519c3c06200e259967199dc02b1e32ff30ddf7
Dan Volchek <danvolchek@users.noreply.github.com>
2021-11-30 20:37:51 -0800

    2019/9: add solution

---
+++ 2019/9/vm/ops.go
@@ -0,0 +1,50 @@
+package vm
+
+
+func add(vm *VM, arg0, arg1, arg2 arg) {
+	arg2.write(arg0.read() + arg1.read())
+}
+
+func mul(vm *VM, arg0, arg1, arg2 arg) {
+	arg2.write(arg0.read() * arg1.read())
+}
+
+func in(vm *VM, arg0 arg) {
+	arg0.write(vm.read())
+}
+
+func out(vm *VM, arg0 arg) {
+	vm.write(arg0.read())
+}
+
+func jit(vm *VM, arg0, arg1 arg) {
+	if arg0.read() != 0 {
+		vm.PC = arg1.read()
+	}
+}
+
+func jif(vm *VM, arg0, arg1 arg) {
+	if arg0.read() == 0 {
+		vm.PC = arg1.read()
+	}
+}
+
+func lt(vm *VM, arg0, arg1, arg2 arg) {
+	val := 0
+	if arg0.read() < arg1.read() {
+		val = 1
+	}
+	arg2.write(val)
+}
+
+func eq(vm *VM, arg0, arg1, arg2 arg) {
+	val := 0
+	if arg0.read() == arg1.read() {
+		val = 1
+	}
+	arg2.write(val)
+}
+
+func rel(vm *VM, arg0 arg) {
+	vm.RelativeBase += arg0.read()
+}
---
+++ 2019/9/vm/types.go
@@ -0,0 +1,82 @@
+package vm
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	positionMode  = 0
+	immediateMode = 1
+	relativeMode  = 2
+)
+
+type instruction int
+
+func (i instruction) Code() int {
+	return int(i) % 100
+}
+
+func (i instruction) Mode(p int) int {
+	v := int(i)
+
+	return (v / int(math.Pow(10, float64(p+2)))) % 10
+}
+
+type arg interface {
+	read() int
+	write(v int)
+}
+
+type immediateArg struct {
+	back int
+}
+
+func (i immediateArg) read() int {
+	return i.back
+}
+
+func (i immediateArg) write(v int) {
+	panic("can't write immediate mode arg")
+}
+
+func (i immediateArg) String() string {
+	return fmt.Sprintf("i(%v)", i.back)
+}
+
+type positionArg struct {
+	raw int
+
+	back *int
+}
+
+func (p positionArg) read() int {
+	return *p.back
+}
+
+func (p positionArg) write(v int) {
+	*p.back = v
+}
+
+func (p positionArg) String() string {
+	return fmt.Sprintf("p(%v -> %v)", p.raw, *p.back)
+}
+
+type relativeArg struct {
+	raw int
+	base int
+
+	back *int
+}
+
+func (r relativeArg) read() int {
+	return *r.back
+}
+
+func (r relativeArg) write(v int) {
+	*r.back = v
+}
+
+func (r relativeArg) String() string {
+	return fmt.Sprintf("r(%v + %v -> %v)", r.raw, r.base, *r.back)
+}
