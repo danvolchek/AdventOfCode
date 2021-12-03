288ef7e14def8819ab2df0fccff6cb16b25d7c8b
Dan Volchek <danvolchek@users.noreply.github.com>
2021-12-02 21:19:02 -0800

    2021/3: add initial solution, input

--- 2021/3/naughty/1/main.go
+++ 2021/3/naughty/1/main.go
@@ -17,14 +17,40 @@ func input() *os.File {
 	return input
 }
 
+const width = 12
+
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	var sum []int = make([]int, width)
 	for scanner.Scan() {
 		row := scanner.Text()
 
-		fmt.Println(row)
+		//fmt.Println(row)
+
+		for i, v := range row {
+			if v == '0' {
+				sum[i] -= 1
+			} else {
+				sum[i] += 1
+			}
+		}
 	}
+
+	gamma := 0
+	eps := 0
+
+	fmt.Printf("%+v\n", sum)
+
+	for i, v := range sum {
+		if v > 0 {
+			gamma += (1 << (width - i - 1))
+		} else {
+			eps += (1 << (width - i - 1))
+		}
+	}
+
+	fmt.Println(gamma, eps, gamma*eps)
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
@@ -32,5 +58,6 @@ func solve(r io.Reader) {
 }
 
 func main() {
+	//solve(strings.NewReader("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"))
 	solve(input())
 }
--- 2021/3/naughty/2/main.go
+++ 2021/3/naughty/2/main.go
@@ -17,20 +17,94 @@ func input() *os.File {
 	return input
 }
 
+const width = 12
+
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	var vals []string
 	for scanner.Scan() {
 		row := scanner.Text()
 
-		fmt.Println(row)
+		vals = append(vals, row)
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	vals2 := make([]string, len(vals))
+	copy(vals2, vals)
+
+	pos := 0
+	for len(vals) != 1 {
+		common := count(vals)
+
+		if common[pos] >= 0 {
+			vals = discard(vals, pos, '0')
+		} else {
+			vals = discard(vals, pos, '1')
+		}
+
+		pos += 1
+	}
+
+	pos = 0
+	for len(vals2) != 1 {
+		common := count(vals2)
+
+		if common[pos] < 0 {
+			vals2 = discard(vals2, pos, '0')
+		} else {
+			vals2 = discard(vals2, pos, '1')
+		}
+
+		pos += 1
+	}
+
+	fmt.Println(vals, vals2, toInt(vals[0]), toInt(vals2[0]), toInt(vals[0])*toInt(vals2[0]))
+
+}
+
+func toInt(v string) int {
+	sum := 0
+	for i, vv := range v {
+		if vv == '1' {
+			sum += (1 << (width - i - 1))
+		}
+	}
+
+	return sum
+}
+
+func discard(vals []string, i int, w uint8) []string {
+	var ret []string
+	for _, v := range vals {
+		if v[i] != w {
+			ret = append(ret, v)
+		}
+	}
+
+	return ret
+}
+
+func count(nums []string) []int {
+	var sum []int = make([]int, width)
+
+	for _, row := range nums {
+		for i, v := range row {
+			if v == '0' {
+				sum[i] -= 1
+			} else {
+				sum[i] += 1
+			}
+		}
+	}
+
+	return sum
 }
 
 func main() {
+	//solve(strings.NewReader("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"))
 	solve(input())
 }
