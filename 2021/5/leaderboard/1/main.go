5205dfbd2360593ea8920f5173a2245b34696bbc
Dan Volchek <danvolchek@users.noreply.github.com>
2021-12-04 21:33:35 -0800

    2021/5: add input, leaderboard solution

--- 2021/5/leaderboard/1/main.go
+++ 2021/5/leaderboard/1/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 )
 
 func input() *os.File {
@@ -17,20 +19,91 @@ func input() *os.File {
 	return input
 }
 
+type point struct {
+	x, y int
+}
+
+func parsePoint(rawPoint string) point {
+	parts := strings.Split(rawPoint, ",")
+
+	x, err := strconv.Atoi(parts[0])
+	if err != nil {
+		panic(err)
+	}
+
+	y, err := strconv.Atoi(parts[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return point{
+		x: x,
+		y: y,
+	}
+}
+
+func parseLine(rawLine string) (point, point) {
+	parts := strings.Split(rawLine, " -> ")
+
+	return parsePoint(parts[0]), parsePoint(parts[1])
+}
+
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
+
+	field := make(map[point]int)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		start, end := parseLine(line)
+
+		walk(start, end, func(p point) {
+			field[p] += 1
+		})
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	overlap := 0
+	for _, v := range field {
+		if v > 1 {
+			overlap += 1
+		}
+	}
+
+	fmt.Println(overlap)
+}
+
+func walk(start, end point, action func(p point)) {
+	if start.y == end.y {
+		if start.x > end.x {
+			start, end = end, start
+		}
+
+		for x := start.x; x <= end.x; x++ {
+			action(point{
+				x: x,
+				y: start.y,
+			})
+		}
+	} else if start.x == end.x {
+		if start.y > end.y {
+			start, end = end, start
+		}
+
+		for y := start.y; y <= end.y; y++ {
+			action(point{
+				x: start.x,
+				y: y,
+			})
+		}
+	}
 }
 
 func main() {
+	solve(strings.NewReader("0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2\n"))
 	solve(input())
 }
