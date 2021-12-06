101ef0d5cd4208a907b90b5ef5d58a31f66830b1
Dan Volchek <danvolchek@users.noreply.github.com>
2021-12-05 23:55:17 -0800

    2021/6: add input, leaderboard solutions

--- 2021/6/leaderboard/1/main.go
+++ 2021/6/leaderboard/1/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 )
 
 func input() *os.File {
@@ -20,17 +22,47 @@ func input() *os.File {
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	var fish []int
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		fishStr := strings.Split(line, ",")
+		for _, f := range fishStr {
+			v, err := strconv.Atoi(f)
+			if err != nil {
+				panic(err)
+			}
+
+			fish = append(fish, v)
+		}
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	for i := 0; i < 80; i++ {
+		var additions []int
+
+		for i, f := range fish {
+			if f != 0 {
+				fish[i]--
+			} else {
+				fish[i] = 6
+				additions = append(additions, 8)
+			}
+		}
+
+		for _, add := range additions {
+			fish = append(fish, add)
+		}
+	}
+
+	fmt.Println(len(fish))
+
 }
 
 func main() {
+	solve(strings.NewReader("3,4,3,1,2"))
 	solve(input())
 }
--- 2021/6/leaderboard/2/main.go
+++ 2021/6/leaderboard/2/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 )
 
 func input() *os.File {
@@ -20,17 +22,48 @@ func input() *os.File {
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	fish := make(map[int]int)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		fishStr := strings.Split(line, ",")
+		for _, f := range fishStr {
+			v, err := strconv.Atoi(f)
+			if err != nil {
+				panic(err)
+			}
+
+			fish[v] += 1
+		}
 	}
 
+	//fmt.Println(fish)
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	for i := 0; i < 256; i++ {
+
+		fish[9] = fish[0]
+		fish[7] += fish[0]
+
+		for i := 0; i < 9; i++ {
+			fish[i] = fish[i+1]
+		}
+		//fmt.Println(fish)
+	}
+
+	sum := 0
+
+	for i := 0; i < 9; i++ {
+		sum += fish[i]
+	}
+
+	fmt.Println(sum)
+
 }
 
 func main() {
+	solve(strings.NewReader("3,4,3,1,2"))
 	solve(input())
 }
