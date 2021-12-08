5daba86c090d7c074f4823c9bbedcfbaf7474d22
Dan Volchek <danvolchek@users.noreply.github.com>
2021-12-07 21:56:51 -0800

    2021/7: add better optimized solution for part 1

--- 2021/7/optimized/1/main.go
+++ 2021/7/optimized/1/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -18,14 +19,13 @@ func input() *os.File {
 	return input
 }
 
-func parse(r io.Reader) ([]int, int, int) {
+func parse(r io.Reader) []int {
 	line, err := io.ReadAll(r)
 	if err != nil {
 		panic(err)
 	}
 
 	var crabs []int
-	minCrab, maxCrab := -1, -1
 
 	rawCrabs := strings.Split(strings.TrimSpace(string(line)), ",")
 	for _, rawCrab := range rawCrabs {
@@ -33,32 +33,26 @@ func parse(r io.Reader) ([]int, int, int) {
 		if err != nil {
 			panic(err)
 		}
+
 		crabs = append(crabs, crab)
-
-		if minCrab < 0 || crab < minCrab {
-			minCrab = crab
-		}
-
-		if maxCrab < 0 || crab > maxCrab {
-			maxCrab = crab
-		}
 	}
 
-	return crabs, minCrab, maxCrab
+	return crabs
 }
 
 func solve(r io.Reader) {
-	crabs, minCrab, maxCrab := parse(r)
+	crabs := parse(r)
 
-	minFuel := -1
-	for position := minCrab; position <= maxCrab; position += 1 {
-		fuel := totalFuel(position, crabs)
-		if minFuel < 0 || fuel < minFuel {
-			minFuel = fuel
-		}
-	}
+	sort.Slice(crabs, func(i, j int) bool {
+		return crabs[i] < crabs[j]
+	})
 
-	fmt.Println(minFuel)
+	// The median is the optimal position to move to. See proof below.
+	optimalPosition := crabs[len(crabs)/2]
+
+	minFuel := totalFuel(optimalPosition, crabs)
+
+	fmt.Println(minFuel, optimalPosition)
 }
 
 func totalFuel(position int, crabs []int) int {
@@ -82,3 +76,38 @@ func main() {
 	solve(strings.NewReader("16,1,2,0,4,2,7,1,2,14"))
 	solve(input())
 }
+
+/*
+Why the median position is the one which would spend the least fuel:
+
+
+For an odd numbered amount of crabs:
+
+Consider the case of moving to the median position, m.
+Exactly half the crabs are to the left (at position < m) and half the crabs are to the right (at position > m).
+
+Now consider the case of moving to m-1, with left crabs being at position < m - 1 and right crabs being at position > m - 1.
+Because m is the median, there are more right crabs than there are left crabs.
+
+Each left crab has to spend 1 fuel less to get to m - 1 as compared to get to m.
+
+Each right crab has to spend 1 fuel more to get to m - 1 as compared to get to m.
+
+Because there are more right crabs than left crabs, the increase in distance needed to move by the right crabs outweighs
+the decrease in distance needed to move by the left crabs.
+Therefore, the net fuel used is more than moving to m.
+
+The same logic holds for any where m - k: there will be more right crabs than left crabs, so the additional distance the right crabs have to move will dominate the left crabs.
+
+The argument is the same for m + k: now there are more left crabs than right crabs.
+
+
+For an even numbered amount of crabs:
+
+Let m1 and m2 be the two middle values, where m1 and m2 where m2 > m1. Consider a point m3 where m3 = (m1 + m2) / 2.
+
+Add this crab to the list between m1 and m2. The list has an odd number of crabs now, and m3 is the median. By the above
+proof, m3 is the optimal position. Now consider
+
+<not complete>
+*/
