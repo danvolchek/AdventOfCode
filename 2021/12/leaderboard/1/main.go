bce8b83b1a3b674478480d8019ea77a55b20d2e0
Dan Volchek <danvolchek@users.noreply.github.com>
2021-12-12 00:02:51 -0800

    2021/12: add leaderboard solution

--- 2021/12/leaderboard/1/main.go
+++ 2021/12/leaderboard/1/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"path"
+	"strings"
+	"time"
 )
 
 func input() *os.File {
@@ -20,17 +22,80 @@ func input() *os.File {
 func solve(r io.Reader) {
 	scanner := bufio.NewScanner(r)
 
+	caves := make(map[string][]string)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		parts := strings.Split(line, "-")
+		a := parts[0]
+		b := parts[1]
+
+		caves[a] = append(caves[a], b)
+		caves[b] = append(caves[b], a)
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
+
+	t := time.Now()
+	result := explore(caves, "start", nil)
+	fmt.Println(len(result))
+	fmt.Println(time.Now().Sub(t))
+}
+
+func explore(caves map[string][]string, current string, path []string) [][]string {
+	next := append(clone(path), current)
+	if current == "end" {
+		return [][]string{next}
+	}
+
+	var result [][]string
+	for _, cave := range caves[current] {
+		if transitionedBefore(current, cave, next) || visitingSmallAgain(cave, next) {
+			continue
+		}
+
+		for _, newPath := range explore(caves, cave, next) {
+			result = append(result, newPath)
+		}
+	}
+
+	return result
+}
+
+func transitionedBefore(from, to string, path []string) bool {
+	for i := 0; i < len(path)-1; i++ {
+		if path[i] == from && path[i+1] == to {
+			return true
+		}
+	}
+
+	return false
+}
+
+func visitingSmallAgain(cave string, path []string) bool {
+	if strings.ToLower(cave) != cave {
+		return false
+	}
+
+	for _, visitedCave := range path {
+		if visitedCave == cave {
+			return true
+		}
+	}
+
+	return false
+}
+
+func clone(path []string) []string {
+	ret := make([]string, len(path))
+	copy(ret, path)
+	return ret
 }
 
 func main() {
+	solve(strings.NewReader("start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end"))
+	solve(strings.NewReader("fs-end\nhe-DX\nfs-he\nstart-DX\npj-DX\nend-zg\nzg-sl\nzg-pj\npj-he\nRW-he\nfs-DX\npj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW"))
 	solve(input())
 }
