6d0cf62004b155e894ed82b269383ccfd3626ce9
Dan Volchek <danvolchek@users.noreply.github.com>
2022-11-14 22:38:26 -0800

    2015/6: add lb solution

---
+++ 2015/6/leaderboard/1/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"github.com/danvolchek/AdventOfCode/lib"
+	"os"
+	"path"
+	"regexp"
+)
+
+func input() *os.File {
+	input, err := os.Open(path.Join("2015", "6", "input.txt"))
+	if err != nil {
+		panic(err)
+	}
+
+	return input
+}
+
+type action int
+
+const (
+	on action = iota
+	off
+	toggle
+)
+
+type pos struct {
+	x, y int
+}
+
+type instruction struct {
+	act        action
+	start, end pos
+}
+
+var lineRegexp = regexp.MustCompile(`(.+) (\d+),(\d+) through (\d+),(\d+)`)
+
+func parse(line string) instruction {
+	result := lineRegexp.FindAllStringSubmatch(line, -1)
+
+	parts := result[0][1:]
+
+	var act action
+	switch parts[0] {
+	case "turn on":
+		act = on
+	case "turn off":
+		act = off
+	case "toggle":
+		act = toggle
+	default:
+		panic(parts[0])
+	}
+
+	return instruction{
+		act: act,
+		start: pos{
+			x: lib.Atoi(parts[1]),
+			y: lib.Atoi(parts[2]),
+		},
+		end: pos{
+			x: lib.Atoi(parts[3]),
+			y: lib.Atoi(parts[4]),
+		},
+	}
+}
+
+func solve(instructions []instruction) int {
+	grid := make(map[pos]bool)
+
+	for _, instr := range instructions {
+		switch instr.act {
+		case on:
+			for x := instr.start.x; x <= instr.end.x; x += 1 {
+				for y := instr.start.y; y <= instr.end.y; y += 1 {
+					grid[pos{x: x, y: y}] = true
+				}
+			}
+		case off:
+			for x := instr.start.x; x <= instr.end.x; x += 1 {
+				for y := instr.start.y; y <= instr.end.y; y += 1 {
+					grid[pos{x: x, y: y}] = false
+				}
+			}
+		case toggle:
+			for x := instr.start.x; x <= instr.end.x; x += 1 {
+				for y := instr.start.y; y <= instr.end.y; y += 1 {
+					grid[pos{x: x, y: y}] = !grid[pos{x: x, y: y}]
+				}
+			}
+		default:
+			panic(instr.act)
+		}
+	}
+
+	totalLit := 0
+	for _, isLit := range grid {
+		if isLit {
+			totalLit += 1
+		}
+	}
+
+	return totalLit
+}
+
+func main() {
+	lib.TestSolveParseLines("turn on 0,0 through 999,999", parse, solve)
+	lib.TestSolveParseLines("toggle 0,0 through 999,0", parse, solve)
+	lib.TestSolveParseLines("turn on 499,499 through 500,500", parse, solve)
+	lib.SolveParseLines(input(), parse, solve)
+}
