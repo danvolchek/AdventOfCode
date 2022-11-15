d4c6692f3a2dad10430f954ead9d9423c6fb3a8c
Dan Volchek <danvolchek@users.noreply.github.com>
2022-11-14 21:54:05 -0800

    lib: add helper solve functions

--- lib/lib.go
+++ lib/lib.go
@@ -1,5 +1,12 @@
 package lib
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
 // Must return value if err is non-nil and panics otherwise.
 func Must[T any](value T, err error) T {
 	if err != nil {
@@ -20,4 +27,37 @@ func Min(values ...int) int {
 	}
 
 	return min
+}
+
+// SolveBytes runs solution using the raw result of reading r.
+func SolveBytes[T any](r io.Reader, solution func(input []byte) T) {
+	input := Must(io.ReadAll(r))
+	result := solution(input)
+	fmt.Println(result)
+}
+
+// TestSolveBytes is SolveBytes with a string input (for test cases).
+func TestSolveBytes[T any](input string, solution func(input []byte) T) {
+	SolveBytes(strings.NewReader(input), solution)
+}
+
+// SolveLines runs solution interpreting r as a newline delimited list of strings.
+func SolveLines[T any](r io.Reader, solution func(lines []string) T) {
+	var lines []string
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
+
+	result := solution(lines)
+	fmt.Println(result)
+}
+
+// TestSolveLines is SolveLines with a string input (for test cases).
+func TestSolveLines[T any](input string, solution func(lines []string) T) {
+	SolveLines(strings.NewReader(input), solution)
 }
