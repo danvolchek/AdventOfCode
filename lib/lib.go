85ebd8dd61771efabf7c6a12777a92338640bfc3
Dan Volchek <danvolchek@users.noreply.github.com>
2022-11-14 22:03:58 -0800

    lib: add a varient of SolveLines that allows a custom parse function

--- lib/lib.go
+++ lib/lib.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
@@ -29,6 +30,11 @@ func Min(values ...int) int {
 	return min
 }
 
+// Atoi is a convenience wrapper on [strconv.Atoi] that panics if it fails.
+func Atoi(s string) int {
+	return Must(strconv.Atoi(s))
+}
+
 // SolveBytes runs solution using the raw result of reading r.
 func SolveBytes[T any](r io.Reader, solution func(input []byte) T) {
 	input := Must(io.ReadAll(r))
@@ -43,11 +49,21 @@ func TestSolveBytes[T any](input string, solution func(input []byte) T) {
 
 // SolveLines runs solution interpreting r as a newline delimited list of strings.
 func SolveLines[T any](r io.Reader, solution func(lines []string) T) {
-	var lines []string
+	SolveParseLines(r, func(line string) string { return line }, solution)
+}
+
+// TestSolveLines is SolveLines with a string input (for test cases).
+func TestSolveLines[T any](input string, solution func(lines []string) T) {
+	SolveLines(strings.NewReader(input), solution)
+}
+
+// SolveParseLines runs solution interpreting r as a newline delimited list of strings, parsed according to a function.
+func SolveParseLines[T, K any](r io.Reader, parse func(line string) K, solution func(lines []K) T) {
+	var lines []K
 
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		lines = append(lines, parse(scanner.Text()))
 	}
 	if scanner.Err() != nil {
 		panic(scanner.Err())
@@ -57,7 +73,7 @@ func SolveLines[T any](r io.Reader, solution func(lines []string) T) {
 	fmt.Println(result)
 }
 
-// TestSolveLines is SolveLines with a string input (for test cases).
-func TestSolveLines[T any](input string, solution func(lines []string) T) {
-	SolveLines(strings.NewReader(input), solution)
+// TestSolveParseLines is SolveParseLines with a string input (for test cases).
+func TestSolveParseLines[T, K any](input string, parse func(line string) K, solution func(lines []K) T) {
+	SolveParseLines(strings.NewReader(input), parse, solution)
 }
