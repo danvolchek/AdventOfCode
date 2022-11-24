57d898a456888ca78d42e8053ed037c149018536
Dan Volchek <danvolchek@users.noreply.github.com>
2022-11-23 21:36:41 -0800

    2015/19: add part 2 attempt

--- 2015/19/leaderboard/2/main.go
+++ 2015/19/leaderboard/2/main.go
@@ -1,30 +1,104 @@
 package main
 
 import (
-	"fmt"
 	"github.com/danvolchek/AdventOfCode/lib"
+	"strings"
 )
 
-func parse(line string) any {
-	return line
+type Replacement struct {
+	in, out string
 }
 
-func solve(lines []any) int {
-	for _, line := range lines {
+type Input struct {
+	replacements []Replacement
+	target       string
+}
 
-		fmt.Println(line)
+func parse(data string) Input {
+	before, after, ok := strings.Cut(data, "\n\n")
+	if !ok {
+		panic(ok)
 	}
 
-	return 0
+	input := Input{
+		replacements: nil,
+		target:       strings.TrimSpace(after),
+	}
+
+	for _, line := range strings.Split(before, "\n") {
+		parts := strings.Split(line, " => ")
+		input.replacements = append(input.replacements, Replacement{in: parts[0], out: parts[1]})
+	}
+
+	return input
+}
+
+func neighbors(replacements []Replacement, current string) []string {
+	var s lib.OrderedSet[string]
+
+	for _, replacement := range replacements {
+		for i := 0; i < len(current)-len(replacement.in)+1; i++ {
+			if current[i:i+len(replacement.in)] == replacement.in {
+				molecule := current[:i] + replacement.out + current[i+len(replacement.in):]
+				s.Add(molecule)
+			}
+		}
+	}
+
+	return s.Items()
+}
+
+type node struct {
+	molecule string
+
+	replacements []Replacement
+}
+
+func (n *node) Id() string {
+	return n.molecule
+}
+
+func (n *node) Adjacent() []*node {
+	neighborMolecules := neighbors(n.replacements, n.molecule)
+	return lib.Map(neighborMolecules, func(molecule string) *node { return &node{molecule: molecule, replacements: n.replacements} })
+}
+
+func explore(input Input) []string {
+	start := &node{
+		molecule:     "e",
+		replacements: input.replacements,
+	}
+
+	return lib.Map(lib.BFS(start, input.target), func(n *node) string { return n.molecule })
+}
+
+func solve(input Input) int {
+	return len(explore(input)) - 1
 }
 
 func main() {
-	solver := lib.Solver[[]any, int]{
-		ParseF: lib.ParseLine(parse),
+	graphSolver := lib.Solver[Input, []string]{
+		ParseF: lib.ParseStringFunc(parse),
+		SolveF: explore,
+	}
+
+	graphSolver.Expect("e => H\ne => O\nH => HO\nH => OH\nO => HH\n\nHOH\n", []string{"e", "O", "HH", "HOH"})
+	graphSolver.Expect("e => H\ne => O\nH => HO\nH => OH\nO => HH\n\nHOHOHO\n", []string{"e", "H", "HO", "HHH", "HHHO", "HHOHO", "HOHOHO"})
+
+	solver := lib.Solver[Input, int]{
+		ParseF: lib.ParseStringFunc(parse),
 		SolveF: solve,
 	}
 
-	solver.Expect("foo", 1)
-	solver.Expect("bar", 2)
-	solver.Solve()
+	solver.ParseExpect("H => HO\nH => OH\n\nHOHOHO\n", Input{
+		replacements: []Replacement{{in: "H", out: "HO"}, {in: "H", out: "OH"}},
+		target:       "HOHOHO",
+	})
+
+	solver.Expect("e => H\ne => O\nH => HO\nH => OH\nO => HH\n\nHOH\n", 3)
+	solver.Expect("e => H\ne => O\nH => HO\nH => OH\nO => HH\n\nHOHOHO\n", 6)
+
+	// Note for me later: the search space is so big that solving the input runs out of memory and the process crashes
+	// the search space needs to be reduced somehow, or the problem be formulated not as graph search.
+	// solver.Solve()
 }
