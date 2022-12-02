4cd26c2ed809c4c405099897a0b5034f5ba7cd08
Dan Volchek <danvolchek@users.noreply.github.com>
2022-12-01 20:49:19 -0800

    2022/{1,2}, cmd/template: use tabs instead of spaces

--- 2022/2/leaderboard/1/main.go
+++ 2022/2/leaderboard/1/main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"github.com/danvolchek/AdventOfCode/lib"
 )
 
 func parse(line string) any {
-    return line
+	return line
 }
 
 func solve(lines []any) int {
 	for _, line := range lines {
 
-        fmt.Println(line)
-    }
+		fmt.Println(line)
+	}
 
-    return 0
+	return 0
 }
 
 func main() {
 	solver := lib.Solver[[]any, int]{
-    		ParseF: lib.ParseLine(parse),
-    		SolveF: solve,
-    	}
+		ParseF: lib.ParseLine(parse),
+		SolveF: solve,
+	}
 
-    solver.Expect("foo", 1)
-    solver.Expect("bar", 2)
-    solver.Solve()
+	solver.Expect("foo", 1)
+	solver.Solve()
 }
