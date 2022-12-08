f2c813b025f849c237522106fb058876d9a6f449
Dan Volchek <danvolchek@users.noreply.github.com>
2022-12-07 21:16:36 -0800

    2022/8: add lb solutions

--- 2022/8/leaderboard/1/main.go
+++ 2022/8/leaderboard/1/main.go
@@ -1,29 +1,88 @@
 package main
 
 import (
-	"fmt"
 	"github.com/danvolchek/AdventOfCode/lib"
+	"strings"
 )
 
-func parse(line string) any {
-	return line
-}
+func parse(input string) [][]int {
+	var result [][]int
 
-func solve(lines []any) int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
 	for _, line := range lines {
+		parts := strings.Split(line, "")
 
-		fmt.Println(line)
+		var ret []int
+
+		for _, p := range parts {
+			ret = append(ret, lib.Ints(p)[0])
+		}
+
+		result = append(result, ret)
 	}
 
-	return 0
+	return result
+}
+
+func isVisible(x, y int, grid [][]int) bool {
+	v := grid[x][y]
+
+	visibleX1 := true
+	for i := x - 1; i > -1; i-- {
+		if grid[i][y] >= v {
+			visibleX1 = false
+			break
+		}
+	}
+
+	visibleX2 := true
+	for i := x + 1; i < len(grid); i++ {
+		if grid[i][y] >= v {
+			visibleX2 = false
+			break
+		}
+	}
+
+	visibleY1 := true
+	for i := y - 1; i > -1; i-- {
+		if grid[x][i] >= v {
+			visibleY1 = false
+			break
+		}
+	}
+
+	visibleY2 := true
+	for i := y + 1; i < len(grid[x]); i++ {
+		if grid[x][i] >= v {
+			visibleY2 = false
+			break
+		}
+	}
+
+	return visibleX1 || visibleX2 || visibleY1 || visibleY2
+}
+
+func solve(lines [][]int) int {
+
+	sum := 0
+	for x := 0; x < len(lines); x++ {
+		for y := 0; y < len(lines); y++ {
+			if isVisible(x, y, lines) {
+				sum += 1
+			}
+		}
+	}
+
+	return sum
 }
 
 func main() {
-	solver := lib.Solver[[]any, int]{
-		ParseF: lib.ParseLine(parse),
+	solver := lib.Solver[[][]int, int]{
+		ParseF: lib.ParseStringFunc(parse),
 		SolveF: solve,
 	}
 
-	solver.Expect("foo", 1)
+	solver.Expect("30373\n25512\n65332\n33549\n35390\n", 21)
 	solver.Solve()
 }
--- 2022/8/leaderboard/2/main.go
+++ 2022/8/leaderboard/2/main.go
@@ -1,29 +1,87 @@
 package main
 
 import (
-	"fmt"
 	"github.com/danvolchek/AdventOfCode/lib"
+	"strings"
 )
 
-func parse(line string) any {
-	return line
-}
+func parse(input string) [][]int {
+	var result [][]int
 
-func solve(lines []any) int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
 	for _, line := range lines {
+		parts := strings.Split(line, "")
 
-		fmt.Println(line)
+		var ret []int
+
+		for _, p := range parts {
+			ret = append(ret, lib.Ints(p)[0])
+		}
+
+		result = append(result, ret)
 	}
 
-	return 0
+	return result
+}
+
+func viewingDistance(x, y int, grid [][]int) int {
+	v := grid[x][y]
+
+	visibleX1 := 0
+	for i := x - 1; i > -1; i-- {
+		visibleX1 += 1
+		if grid[i][y] >= v {
+			break
+		}
+	}
+
+	visibleX2 := 0
+	for i := x + 1; i < len(grid); i++ {
+		visibleX2 += 1
+		if grid[i][y] >= v {
+			break
+		}
+	}
+
+	visibleY1 := 0
+	for i := y - 1; i > -1; i-- {
+		visibleY1 += 1
+		if grid[x][i] >= v {
+			break
+		}
+	}
+
+	visibleY2 := 0
+	for i := y + 1; i < len(grid[x]); i++ {
+		visibleY2 += 1
+		if grid[x][i] >= v {
+			break
+		}
+	}
+
+	return visibleX1 * visibleX2 * visibleY1 * visibleY2
+}
+
+func solve(lines [][]int) int {
+	maxView := 0
+	for x := 0; x < len(lines); x++ {
+		for y := 0; y < len(lines); y++ {
+			if dist := viewingDistance(x, y, lines); dist > maxView {
+				maxView = dist
+			}
+		}
+	}
+
+	return maxView
 }
 
 func main() {
-	solver := lib.Solver[[]any, int]{
-		ParseF: lib.ParseLine(parse),
+	solver := lib.Solver[[][]int, int]{
+		ParseF: lib.ParseStringFunc(parse),
 		SolveF: solve,
 	}
 
-	solver.Expect("foo", 1)
+	solver.Expect("30373\n25512\n65332\n33549\n35390\n", 8)
 	solver.Solve()
 }
