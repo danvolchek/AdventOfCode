f84df92431739badd93ad0999f16ad58a5538aad
Dan Volchek <danvolchek@users.noreply.github.com>
2022-12-11 22:43:46 -0800

    2022/12: add lb solutions

--- 2022/12/leaderboard/1/main.go
+++ 2022/12/leaderboard/1/main.go
@@ -5,25 +5,108 @@ import (
 	"github.com/danvolchek/AdventOfCode/lib"
 )
 
-func parse(line string) any {
-	return line
+type node struct {
+	x, y int
+
+	adjacent []*node
 }
 
-func solve(lines []any) int {
-	for _, line := range lines {
+func (n *node) Id() string {
+	return fmt.Sprintf("%d,%d", n.y, n.x)
+}
 
-		fmt.Println(line)
+func (n *node) Adjacent() []*node {
+	return n.adjacent
+}
+
+func (n *node) String() string {
+	return n.Id()
+}
+
+func parse(line string) int {
+	if line == "S" {
+		return 9999999
 	}
 
-	return 0
+	if line == "E" {
+		return -2
+	}
+
+	return int(line[0] - 'a')
+}
+
+func solve(grid [][]int) int {
+
+	var start *node
+	var end string
+
+	gridMap := make(map[int]map[int]*node)
+
+	for y, line := range grid {
+		for x, height := range line {
+			n := &node{
+				x:        x,
+				y:        y,
+				adjacent: nil,
+			}
+
+			if height == 9999999 {
+				start = n
+			}
+
+			if height == -2 {
+				end = n.Id()
+			}
+
+			if gridMap[y] == nil {
+				gridMap[y] = make(map[int]*node)
+			}
+
+			gridMap[y][x] = n
+
+			if height == -2 {
+				grid[y][x] = parse("z")
+			}
+		}
+	}
+
+	for y, line := range grid {
+		for x, height := range line {
+			for iy := -1; iy <= 1; iy += 1 {
+				for ix := -1; ix <= 1; ix += 1 {
+					if iy == 0 && ix == 0 {
+						continue
+					}
+					if lib.Abs(iy)+lib.Abs(ix) == 2 {
+						continue
+					}
+
+					ey := y + iy
+					ex := x + ix
+
+					if ey < 0 || ex < 0 || ey >= len(grid) || ex >= len(grid[ey]) {
+						continue
+					}
+
+					if height >= grid[ey][ex]-1 {
+						gridMap[y][x].adjacent = append(gridMap[y][x].adjacent, gridMap[ey][ex])
+					}
+				}
+			}
+		}
+	}
+
+	result := lib.BFS(start, end)
+	fmt.Println(result)
+	return len(result) - 1
 }
 
 func main() {
-	solver := lib.Solver[[]any, int]{
-		ParseF: lib.ParseLine(parse),
+	solver := lib.Solver[[][]int, int]{
+		ParseF: lib.ParseGrid(parse),
 		SolveF: solve,
 	}
 
-	solver.Expect("foo", 1)
-	solver.Solve()
+	solver.Expect("Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi", 31)
+	solver.Verify(330)
 }
--- 2022/12/leaderboard/2/main.go
+++ 2022/12/leaderboard/2/main.go
@@ -5,25 +5,123 @@ import (
 	"github.com/danvolchek/AdventOfCode/lib"
 )
 
-func parse(line string) any {
-	return line
+type node struct {
+	x, y int
+
+	adjacent []*node
 }
 
-func solve(lines []any) int {
-	for _, line := range lines {
+func (n *node) Id() string {
+	return fmt.Sprintf("%d,%d", n.y, n.x)
+}
 
-		fmt.Println(line)
+func (n *node) Adjacent() []*node {
+	return n.adjacent
+}
+
+func (n *node) String() string {
+	return n.Id()
+}
+
+func parse(line string) int {
+	if line == "S" {
+		return 9999999
 	}
 
-	return 0
+	if line == "E" {
+		return -2
+	}
+
+	return int(line[0] - 'a')
+}
+
+func solve(grid [][]int) int {
+
+	var starts []*node
+	var end string
+
+	gridMap := make(map[int]map[int]*node)
+
+	for y, line := range grid {
+		for x, height := range line {
+			n := &node{
+				x:        x,
+				y:        y,
+				adjacent: nil,
+			}
+
+			if height == 9999999 {
+				starts = append(starts, n)
+			}
+
+			if height == -2 {
+				end = n.Id()
+			}
+
+			if height == 0 {
+				starts = append(starts, n)
+			}
+
+			if gridMap[y] == nil {
+				gridMap[y] = make(map[int]*node)
+			}
+
+			gridMap[y][x] = n
+
+			if height == -2 {
+				grid[y][x] = parse("z")
+			}
+
+			if height == 9999999 {
+				grid[y][x] = parse("a")
+			}
+		}
+	}
+
+	for y, line := range grid {
+		for x, height := range line {
+			for iy := -1; iy <= 1; iy += 1 {
+				for ix := -1; ix <= 1; ix += 1 {
+					if iy == 0 && ix == 0 {
+						continue
+					}
+					if lib.Abs(iy)+lib.Abs(ix) == 2 {
+						continue
+					}
+
+					ey := y + iy
+					ex := x + ix
+
+					if ey < 0 || ex < 0 || ey >= len(grid) || ex >= len(grid[ey]) {
+						continue
+					}
+
+					if height >= grid[ey][ex]-1 {
+						gridMap[y][x].adjacent = append(gridMap[y][x].adjacent, gridMap[ey][ex])
+					}
+				}
+			}
+		}
+	}
+
+	distances := lib.Map(starts, func(s *node) (ret int) {
+		defer func() {
+			if r := recover(); r != nil {
+				ret = 9999999999999999
+			}
+		}()
+		return len(lib.BFS(s, end)) - 1
+	})
+
+	return lib.MinSlice(distances)
 }
 
 func main() {
-	solver := lib.Solver[[]any, int]{
-		ParseF: lib.ParseLine(parse),
+	solver := lib.Solver[[][]int, int]{
+		ParseF: lib.ParseGrid(parse),
 		SolveF: solve,
 	}
 
-	solver.Expect("foo", 1)
-	solver.Solve()
+	solver.Expect("Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi", 29)
+	solver.Verify(321)
 }
