1c2f7667c8d26f38b31fb26d2e1718ec075ffebc
Dan Volchek <danvolchek@users.noreply.github.com>
2022-12-12 22:12:52 -0800

    2022/13: add solutions

--- 2022/13/leaderboard/1/main.go
+++ 2022/13/leaderboard/1/main.go
@@ -3,27 +3,135 @@ package main
 import (
 	"fmt"
 	"github.com/danvolchek/AdventOfCode/lib"
+	"strconv"
 )
 
-func parse(line string) any {
-	return line
+type Value struct {
+	Val   int
+	Items []Value
 }
 
-func solve(lines []any) int {
-	for _, line := range lines {
+func (v Value) Int() bool {
+	return v.Items == nil
+}
 
-		fmt.Println(line)
+type Packet struct {
+	val Value
+}
+
+func parts(line string) []string {
+	nesting := 0
+
+	var parts []string
+
+	var last int
+
+	for i := range line {
+		switch line[i] {
+		case '[':
+			nesting += 1
+		case ']':
+			nesting -= 1
+		case ',':
+			if nesting == 0 {
+				parts = append(parts, line[last:i])
+				last = i + 1
+			}
+		}
+	}
+	parts = append(parts, line[last:])
+
+	return parts
+}
+
+func parseValue(line string) Value {
+	if v, err := strconv.Atoi(line); err == nil {
+		return Value{Val: v}
 	}
 
-	return 0
+	stripped := line[1 : len(line)-1]
+
+	p := parts(stripped)
+
+	return Value{
+		Items: lib.Map(lib.Filter(p, func(p string) bool {
+			return len(p) > 0
+		}), parseValue),
+	}
+
+}
+
+func parsePacket(line string) Packet {
+	return Packet{val: parseValue(line)}
+}
+
+func parseChunk(chunk string) []Packet {
+	return lib.ParseLine(parsePacket)(chunk)
+}
+
+func smallerLists(p1, p2 []Value) (bool, bool) {
+	i := 0
+
+	for {
+		if i >= len(p1) {
+			return true, len(p1) != len(p2)
+		}
+
+		if i >= len(p2) {
+			return false, true
+		}
+
+		sm, ok := smaller(p1[i], p2[i])
+		if ok {
+			return sm, true
+		}
+
+		i += 1
+	}
+}
+
+func smaller(p1, p2 Value) (bool, bool) {
+	if p1.Int() && p2.Int() {
+		return p1.Val < p2.Val, p1.Val != p2.Val
+	}
+
+	if !p1.Int() && !p2.Int() {
+		return smallerLists(p1.Items, p2.Items)
+	}
+
+	if p1.Int() {
+		return smallerLists([]Value{p1}, p2.Items)
+	}
+
+	return smallerLists(p1.Items, []Value{p2})
+}
+
+func solve(packets [][]Packet) int {
+
+	sm := lib.Map(packets, func(pair []Packet) bool {
+		sm, ok := smaller(pair[0].val, pair[1].val)
+
+		return sm && ok
+	})
+
+	sum := 0
+
+	for i, item := range sm {
+		if item {
+			fmt.Println(i + 1)
+			sum += i + 1
+		}
+	}
+
+	return sum
 }
 
 func main() {
-	solver := lib.Solver[[]any, int]{
-		ParseF: lib.ParseLine(parse),
+	solver := lib.Solver[[][]Packet, int]{
+		ParseF: lib.ParseChunks(parseChunk),
 		SolveF: solve,
 	}
 
-	solver.Expect("foo", 1)
+	solver.Expect("[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]", 13)
 	solver.Solve()
 }
---
+++ 2022/13/optimized/1/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"github.com/danvolchek/AdventOfCode/lib"
+	"strconv"
+)
+
+type Ordering int
+
+const (
+	Smaller Ordering = iota
+	Equal
+	Larger
+)
+
+type Value struct {
+	Val   int
+	Items []Value
+}
+
+// elements returns the items rawList represents
+func elements(rawList string) []string {
+	if len(rawList) == 0 {
+		return nil
+	}
+
+	var result []string
+
+	var last int
+	var nesting int
+
+	for i := range rawList {
+		switch rawList[i] {
+		case '[':
+			nesting += 1
+		case ']':
+			nesting -= 1
+		case ',':
+			if nesting == 0 {
+				result = append(result, rawList[last:i])
+				last = i + 1
+			}
+		}
+	}
+	result = append(result, rawList[last:])
+
+	return result
+}
+
+func parseValue(raw string) Value {
+	// If raw is an int, then it is a single value
+	if v, err := strconv.Atoi(raw); err == nil {
+		return Value{Val: v}
+	}
+
+	// Otherwise, strip the list markers and parse the item contents recursively
+	return Value{
+		Items: lib.Map(elements(raw[1:len(raw)-1]), parseValue),
+	}
+}
+
+func parse(chunk string) []Value {
+	return lib.ParseLine(parseValue)(chunk)
+}
+
+func (v Value) Compare(o Value) Ordering {
+	// If both are ints, compare their values
+	if v.Items == nil && o.Items == nil {
+		return compare(v.Val, o.Val)
+	}
+
+	toList := func(v Value) []Value {
+		if v.Items != nil {
+			return v.Items
+		}
+
+		return []Value{v}
+	}
+
+	// Otherwise, compare the elements pairwise, replacing an int with a list of that int
+	vList, oList := toList(v), toList(o)
+
+	for i := 0; i < len(vList) && i < len(oList); i += 1 {
+		result := vList[i].Compare(oList[i])
+		if result != Equal {
+			return result
+		}
+	}
+
+	// If one list ran out if items, the ordering is based on list size
+	return compare(len(vList), len(oList))
+}
+
+func compare(a, b int) Ordering {
+	if a == b {
+		return Equal
+	}
+
+	if a < b {
+		return Smaller
+	}
+
+	return Larger
+}
+
+func solve(packets [][]Value) int {
+	sum := 0
+
+	for i, pair := range packets {
+		if pair[0].Compare(pair[1]) == Smaller {
+			sum += i + 1
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	solver := lib.Solver[[][]Value, int]{
+		ParseF: lib.ParseChunks(parse),
+		SolveF: solve,
+	}
+
+	solver.Expect("[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]", 13)
+	solver.Verify(5340)
+}
